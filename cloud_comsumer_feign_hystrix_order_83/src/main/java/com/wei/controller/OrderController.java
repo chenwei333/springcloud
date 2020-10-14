358a863d6b258b32a220b8969e9ec4313238a32a
wei <552112067@qq.com>
2020-10-14 20:59:21 +0800

    使用类的全局降级使用

--- cloud_comsumer_feign_hystrix_order_83/src/main/java/com/wei/controller/OrderController.java
+++ cloud_comsumer_feign_hystrix_order_83/src/main/java/com/wei/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.wei.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wei.result.CommonResponse;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("comsumer")
+@DefaultProperties(defaultFallback = "doTimeOut_global_fallbackHandler") //配置全局的参数
 public class OrderController {
 
     //根据注册数据进行调用
@@ -31,11 +33,14 @@ public class OrderController {
         return new CommonResponse("执行失败 "  );
     }
     @GetMapping("doTimeOut/{id}")
-    @HystrixCommand(fallbackMethod = "doTimeOut_fallbackHandler",
+   /*
+   指定专属的配置
+   @HystrixCommand(fallbackMethod = "doTimeOut_fallbackHandler",
             commandProperties = {
                     //设置超时处理时间,5秒钟时间类正常
                     @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
-            })
+            })*/
+    @HystrixCommand //没有指定特有的，就用上面的配置公共的属性
     public CommonResponse doTimeOut(@PathVariable("id") Integer id) {
         paymentFeignHystrixService.doTimeOut(id);
         return new CommonResponse("执行时间 "  );
@@ -43,5 +48,10 @@ public class OrderController {
 
     public void doTimeOut_fallbackHandler(Integer id){
         System.out.println("我是消费者83，对面服务繁忙，或者我出错" );
+    }
+
+    //定义一个全局的异常处理
+    public void doTimeOut_global_fallbackHandler(Integer id){
+        System.out.println("全局的异常处理" );
     }
 }
